bfe829a4fd70a5a0065e1cb930b9dc6758aa3df4
Swathysuriyababu12 <swathysuryababu@gmail.com>
2023-12-05 00:11:12 +0530

    route paths added

--- routes/mail.js
+++ routes/mail.js
@@ -1,19 +1,19 @@
 const express = require("express");
 const {
   saveSentEmails,
-  //   getEmails,
-  //   saveDraftEmails,
-  //   moveEmailsToBin,
-  //   toggleStarredEmails,
-  //   deleteEmails,
+    getEmails,
+   saveDraftEmails,
+    moveEmailsToBin,
+    toggleStarredEmails,
+    deleteEmails,
 } = require("../controllers/mailController.js");
 const route = express.Router();
 
 route.post("/save", saveSentEmails);
-// route.get("/emails/:type", getEmails);
-// route.post("/save-draft", saveDraftEmails);
-// route.post("/bin", moveEmailsToBin);
-// route.post("/starred", toggleStarredEmails);
-// route.delete("/delete", deleteEmails);
+route.get("/emails/:type", getEmails);
+route.post("/save-draft", saveDraftEmails);
+route.post("/bin", moveEmailsToBin);
+route.post("/starred", toggleStarredEmails);
+route.delete("/delete", deleteEmails);
 
 module.exports = route;
